c6ef9109d3725638df42f6a363636c5fd5615c0f
Seu Nome <salatiel.dev.javascript@gmail.com>
2025-03-05 11:59:19 -0300

    Atualizações para rodar com Waitress e ajustes no código

--- portifolio.py
+++ portifolio.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -82,4 +83,5 @@ def home():
     """
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
